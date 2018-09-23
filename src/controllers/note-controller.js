3b4e356f7757c4eb497ecf0cea392ef5546c79c7
Marcus <marcus.kerr94@gmail.com>
2018-09-23 10:55:50 +0100

    Clear html string array

--- src/controllers/note-controller.js
+++ src/controllers/note-controller.js
@@ -1,9 +1,10 @@
 (function (exports) {
-  function Controller (noteList = NoteList, noteListView = NoteListView, singleNoteView = SingleNoteView) {
+  function Controller (noteList = new NoteList, noteListView = NoteListView, singleNoteView = SingleNoteView) {
     this.noteList = noteList;
     this.noteListView = new noteListView(this.noteList);
     this.singleNoteView = singleNoteView;
     this.app = document.getElementById('app');
+    this.renderNoteList();
   }
 
   Controller.prototype.renderNoteList = function () {
@@ -12,19 +13,19 @@
   
   Controller.prototype.renderSingleNote = function (noteId) {
     return new this.singleNoteView(this.noteList.getNotes()[noteId]).render();
-  }
+  };
+
+  Controller.prototype.addNote = function(noteText) {
+    this.noteList.addNote(noteText);
+  };
 
   exports.Controller = Controller;
 })(this);
 
-var noteList = new NoteList();
-noteList.addNote("This is the first note");
-noteList.addNote("This is the second not and this one has loads of text");
-noteList.addNote("Third note");
-var controller = new Controller(noteList);
-controller.renderNoteList();
+var controller = new Controller();
 
 showNoteOnChangeUrl();
+submitNoteForm();
 
 function showNoteOnChangeUrl() {
   window.addEventListener('hashchange', showClickedNote);
@@ -42,4 +43,16 @@ function showSingleNote(noteId) {
   document
     .getElementById('app')
     .innerHTML = controller.renderSingleNote(noteId) ;
+};
+
+function submitNoteForm() {
+  document.getElementById('noteForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    createNote(document.getElementById('noteText').value);
+  });
+};
+
+function createNote(noteText){
+  controller.addNote(noteText);
+  controller.renderNoteList();
 };
--- src/views/note-list-view.js
+++ src/views/note-list-view.js
@@ -1,14 +1,16 @@
 (function (exports) {
-  function NoteListView (noteList = new NoteList()) {
+  function NoteListView (noteList) {
     this._note_list = noteList;
     this._html_string_arr = [];
   }
 
   NoteListView.prototype.create = function () {
+    this._html_string_arr = [];
     for (var i = 0; i < this._note_list.getNotes().length; i++) {
       this._html_string_arr.push(`<li><a href="#notes/${this._note_list.getNotes()[i].getId()}"><div>${this._note_list.getNotes()[i].read().substring(0,20)}</div></a></li>`);
     }
-    return `<ul>${this._html_string_arr.join('')}</ul>`
+    console.log(this._html_string_arr);
+    return `<ul>${this._html_string_arr.join('')}</ul>`;
   }
 
   exports.NoteListView = NoteListView;
